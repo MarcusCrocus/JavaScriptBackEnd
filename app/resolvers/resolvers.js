3ae6c363f18112d0320dc8c2fc0eb8e801708a17
Marcus <crocushtml@gmail.com>
2023-05-12 13:54:03 +0200

    producto3

---
+++ app/resolvers/resolvers.js
@@ -0,0 +1,36 @@
+// Importar los controladores
+const weeksController = require("../controllers/weeksController");
+const tasksController = require("../controllers/tasksController");
+
+// Crear el objeto resolvers
+const resolvers = {
+  Query: {
+    // Resolver para obtener todas las semanas
+    getWeeks: weeksController.getWeeks,
+    // Resolver para obtener una semana por _id
+    getWeekById: weeksController.getWeekById,
+    // Resolver para obtener todas las tareas
+    getTasks: tasksController.getTasks,
+    // Resolver para obtener una tarea por _id
+    getTaskById: tasksController.getTaskById,
+    // Resolver para obtener todas las tareas por un _id de una semana
+    getTasksByWeek: tasksController.getTasksByWeek,
+  },
+  Mutation: {
+    // Resolver para crear una semana
+    createWeek: weeksController.createWeek,
+    // Resolver para actualizar una semana por _id
+    updateWeek: weeksController.updateWeek,
+    // Resolver para eliminar una semana por _id
+    deleteWeek: weeksController.deleteWeek,
+    // Resolver para crear una tarea
+    createTask: tasksController.createTask,
+    // Resolver para actualizar una tarea por _id
+    updateTask: tasksController.updateTask,
+    // Resolver para eliminar una tarea por _id
+    deleteTask: tasksController.deleteTask,
+  },
+};
+
+// Exportar el objeto resolvers
+module.exports = resolvers;
